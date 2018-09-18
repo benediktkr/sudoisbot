986c506b556f9f24ed4166da9addfed33a92a33c
Benedikt Kristinsson <benedikt@inventati.org>
2018-09-18 16:05:04 +0200

    starting to package this thing

---
+++ README.md
@@ -0,0 +1,7 @@
+# sudoisbot
+
+This is my Telegram bot, that I'm making to scratch my own itches it. It's username is @sudoisbot on Telegram, but it probably won't be of any use to you. 
+
+# License
+
+GPL
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="sudoisbot",
+    version="0.0.1-dev",
+    packages=[
+        'bot',
+    ],
+    license="GPL",
+    long_description=open('README.md').read(),
+)
