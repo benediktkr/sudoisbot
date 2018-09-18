90e06ba584d36d6c9c492c03ef34a69adf045871
Benedikt Kristinsson <benedikt@inventati.org>
2018-09-18 16:33:50 +0200

    install bin/ scripts

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ setup(
         'sudoisbot',
     ],
     license="GPL",
+    description="Telegram bot for sudo.is systems",
     long_description=open('README.md').read(),
     install_requires=[
         'python-telegram-bot',
         'python-daemon',
-    ]
+    ],
+    scripts=[
+        'bin/listener.py',
+        'bin/sendmsg.py',
+    ],
 )
