55804041d8f443d389244e90f975de18e130507f
Benedikt Kristinsson <benedikt@inventati.org>
2020-06-15 23:45:23 +0200

    temper_pub as service again

--- temps/temper_pub.py
+++ temps/temper_pub.py
@@ -4,6 +4,8 @@ import json
 from datetime import datetime
 import platform
 import argparse
+import time
+import sys
 
 import zmq
 from temper.temper import Temper
@@ -17,7 +19,7 @@ from sudoisbot.common import init
 
 class TemperNotConnectedError(Exception): pass
 
-def temper_pub(name, addr):
+def temper_pub(name, addr, sleep):
     os_ = platform.system()
     if not os_.startswith("Linux"):
         raise OSError(f"platform '{os_}' not supported for temper")
@@ -37,47 +39,61 @@ def temper_pub(name, addr):
 
     temper = Temper()
     # this blocks long enough to give socket.connect time to
-    # finish the connection on my network
-    t = temper.read()
-    logger.trace(t)
-    try:
-        data = {
-            'name': name,
-            'temp': t[0]['internal temperature'],
-            'timestamp': datetime.now().isoformat()
-        }
-    except KeyError:
-        # seems to happen intermittently
-        logger.error(t)
-    except IndexError:
-        # temper.read() returned an empty list
-        raise TemperNotConnectedError("no temper device connected")
+    # finish the connection on my network, at most we will lose the
+    # first datapoint which is fine
+    while True:
+        t = temper.read()
+        logger.trace(t)
+        try:
+            data = {
+                'name': name,
+                'temp': t[0]['internal temperature'],
+                'timestamp': datetime.now().isoformat()
+            }
+        except KeyError:
+            # seems to happen intermittently
+            logger.error(t)
+            timer.sleep(sleep)
+        except IndexError:
+            # temper.read() returned an empty list
+            raise TemperNotConnectedError("no temper device connected")
 
-    sdata = json.dumps(data)
+        sdata = json.dumps(data)
 
-    logger.debug(sdata)
-    socket.send_string(f"temp: {sdata}")
+        logger.debug(sdata)
+        socket.send_string(f"temp: {sdata}")
 
-    socket.close()
-    context.destroy()
+        try:
+            time.sleep(sleep)
+        except KeyboardInterrupt:
+            logger.debug("Closing socket and destroying context")
+            socket.close()
+            context.destroy()
+            raise
 
 def main():
     parser = argparse.ArgumentParser(
         description="emit temp data from Temper, designed for cron",
         add_help=False)
     parser.add_argument("--name", help="set temper name")
+    parser.add_argument("--sleep", type=int, default=240)
 
     config, args = init(__name__, parser)
 
     addr = config['addr']
     name = config['name'] if not args.name else args.name
+    sleep = config['sleep'] if not args.sleep else args.sleep
 
-    logger.debug(f"emitting data as '{name}'")
+    logger.debug(f"emitting data as '{name}' every {sleep} secs")
 
     try:
-        temper_pub(name, addr)
+        temper_pub(name, addr, args.sleep)
     except (OSError, TemperNotConnectedError) as e:
         logger.error(e)
+        return 1
+    except KeyboardInterrupt:
+        logger.warning("Caught C-c, exiting..")
+        return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
