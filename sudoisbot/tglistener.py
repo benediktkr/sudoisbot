e0fc6262cb11a2186b21f5d3715f4f84614cdce4
Benedikt Kristinsson <benedikt@lokun.is>
2020-07-05 01:03:51 +0200

    cleaning up tg listener error handling

--- sudoisbot/tglistener.py
+++ sudoisbot/tglistener.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 from time import sleep
 
 from loguru import logger
@@ -15,23 +16,19 @@ if __name__ == '__main__':
 @catch()
 def main():
     config = init(__name__, fullconfig=True)
-    logger.info("starting telegram listener..")
 
     retries = 10
     retry_sleep = 60
     for _ in range(retries):
         try:
+            logger.info("starting telegram listener..")
             listener.listener(config)
-        except (NetworkError) as e:
+            logger.info("Exiting..")
+            sys.exit(0)
+        except NetworkError as e:
             logger.warning(f"start failed: '{e.message}', retrying..")
             sleep(retry_sleep)
             continue
-        except Exception as e:
-            logger.error("eneded up in the catch all")
-            logger.exception(e)
-            logger.error(f"exception was {type(e)}")
-            logger.error("exiting")
-            import sys
-            sys.exit(13)
+
     else:
         logger.error(f"giving up after {retries} tries.")
