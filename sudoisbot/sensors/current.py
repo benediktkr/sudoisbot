f78252dd73722f1b1f74a05df9ce38f5a36ab085
Benedikt Kristinsson <benedikt@lokun.is>
2021-05-10 22:03:00 +0200

    removing sensors from this project since they are their own project now

--- sudoisbot/sensors/current.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from sudoisbot.sensors.sensors import ArduinoCurrentSensor
-
-with ArduinoCurrentSensor(name="test", kind="arduino-current") as ard:
-    for reading in ard.iter_lines():
-        print(reading)
