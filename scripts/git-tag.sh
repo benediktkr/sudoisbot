db9f265483831f89626c4d2d33a974bb7168d0cf
ben <ben@sudo.is>
2022-10-03 15:41:03 +0000

    script to tag commit with the version from poetry (#11)
    Co-authored-by: Ben Kristinsson <ben@sudo.is>
    Reviewed-on: https://git.sudo.is/ben/sudoisbot/pulls/11

---
+++ scripts/git-tag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+git tag v$(poetry version -s)
+
+read -p "push tag? " pushtag
+case $pushtag in
+    n|no|N|NO)
+        echo "not pushing!"
+        exit 0
+        ;;
+    *)
+        echo -n
+esac
+
+echo git push --tags
